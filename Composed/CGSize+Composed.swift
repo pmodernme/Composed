46ed596b7100d451c16597f2192d26397b4c7b26
Zoe Van Brunt <zvb@mac.com>
2019-11-03 09:03:47 -0800

    Added atOrigin and atCenter to CGSize

---
+++ Composed/CGSize+Composed.swift
@@ -0,0 +1,23 @@
+//
+//  CGSize+Composed.swift
+//  Composed
+//
+//  Created by Zoe Van Brunt on 11/3/19.
+//  Copyright © 2019 Zoe Van Brunt. All rights reserved.
+//
+
+import UIKit
+
+public extension CGSize {
+    func atOrigin(_ origin: CGPoint) -> CGRect {
+        return CGRect(origin: origin, size: self)
+    }
+    
+    func atCenter(_ center: CGPoint) -> CGRect {
+        return CGRect(
+            x: center.x - width/2 ,
+            y: center.y - height/2,
+            width: width,
+            height: height)
+    }
+}
--- ComposedTests/ComposedTests.swift
+++ ComposedTests/ComposedTests.swift
@@ -10,25 +10,21 @@ import XCTest
 @testable import Composed
 
 class ComposedTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testSizePlacement() {
+        var result: CGRect = .zero
+        var expected: CGRect = .zero
+        
+        let size = CGSize(width: 100, height: 100)
+        let point = CGPoint(x: 75, y: 75)
+        
+        result = size.atOrigin(point)
+        expected = CGRect(x: 75, y: 75, width: 100, height: 100)
+        
+        XCTAssertEqual(result, expected)
+        
+        result = size.atCenter(point)
+        expected = CGRect(x: 25, y: 25, width: 100, height: 100)
+        
+        XCTAssertEqual(result, expected)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }

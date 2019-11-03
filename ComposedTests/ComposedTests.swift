451da7d9be99a63ffd278da5ff58ede5d095648e
Zoe Van Brunt <zvb@mac.com>
2019-11-03 08:36:22 -0800

    Initial Commit

---
+++ ComposedTests/ComposedTests.swift
@@ -0,0 +1,34 @@
+//
+//  ComposedTests.swift
+//  ComposedTests
+//
+//  Created by Zoe Van Brunt on 11/3/19.
+//  Copyright © 2019 Zoe Van Brunt. All rights reserved.
+//
+
+import XCTest
+@testable import Composed
+
+class ComposedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

a9c9feca92b773b92b0a79ff4c249ef43afe84d3
Zoe Van Brunt <zvb@mac.com>
2019-11-04 09:47:19 -0800

    Add CGPoint offset

---
+++ Composed/CGPoint+Composed.swift
@@ -0,0 +1,18 @@
+//
+//  CGPoint+Composed.swift
+//  Composed
+//
+//  Created by Zoe Van Brunt on 11/4/19.
+//  Copyright © 2019 Zoe Van Brunt. All rights reserved.
+//
+
+import UIKit
+
+extension CGPoint {
+    func offset(by offset: CGPoint) -> CGPoint {
+        return CGPoint(
+            x: x + offset.x,
+            y: y + offset.y
+        )
+    }
+}

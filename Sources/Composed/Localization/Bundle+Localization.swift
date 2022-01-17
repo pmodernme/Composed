444e63e080f06238721b0c7065f1e977ea5902fe
Zoe Van Brunt <zoe@zoelog.io>
2022-01-17 10:23:57 -0800

    Add localization helpers

---
+++ Sources/Composed/Localization/Bundle+Localization.swift
@@ -0,0 +1,21 @@
+//
+//  Bundle+Localization.swift
+//  ZoeLog
+//
+//  Created by Zoe Van Brunt on 7/5/17.
+//  Copyright © 2017 Zoe Van Brunt. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+    public func localizedString(forKey key: String) -> String {
+        return localizedString(forKey: key, value: nil, table: nil)
+    }
+}
+
+extension String {
+	public init(localized key: String) {
+        self.init(stringLiteral: Bundle.main.localizedString(forKey: key))
+	}
+}

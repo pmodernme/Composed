4f18aa771802b309be36606cc34297b4472e17cc
Zoe Van Brunt <zoe@zoelog.io>
2022-01-23 10:20:33 -0800

    Wrap in canImport(UIKit)

--- Sources/Composed/UIKit/UILabel+Convenience.swift
+++ Sources/Composed/UIKit/UILabel+Convenience.swift
@@ -5,9 +5,13 @@
 //  Created by Zoe Van Brunt on 11/24/21.
 //
 
+#if canImport(UIKit)
+
 import UIKit
 
 public extension UILabel {
     var firstBaselineY: CGFloat { frame.origin.y + font.ascender }
     var ascenderLineY: CGFloat { frame.origin.y + font.ascender - font.capHeight }
 }
+
+#endif
